e5c6cad0434e28c8a2ab6328d0eb208ddd9fb646
unknown <pedritrades97@gmail.com>
2026-07-20 16:14:02 -0300

    feature(database): agregar migración de contacto obligatorio

--- backend/Padelito.Infrastructure/Data/Migrations/20260720184546_RequirePersonContactFields.Designer.cs
+++ backend/Padelito.Infrastructure/Data/Migrations/20260720184546_RequirePersonContactFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Padelito.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Padelito.Infrastructure.Data;
 namespace Padelito.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(PadelitoDbContext))]
-    partial class PadelitoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260720184546_RequirePersonContactFields")]
+    partial class RequirePersonContactFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
